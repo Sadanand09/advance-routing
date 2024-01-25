c81e54d2a5538f26434f7b18d74c761ef80dd907
Sadanand09 <sadanandgurude9@gmail.com>
2024-01-25 13:28:07 +0530

    routing-added

--- advance-routing/src/index.js
+++ advance-routing/src/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './view/Home/Home';
+import Posts from './view/Posts/Posts';
+import Readpost from './view/ReadPost/Readpost';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/posts",
+    element: <Posts />,
+  },
+  {
+    path: "/post/:id",
+    element: <Readpost />,
+  },
+  {
+    path: "*",
+    element: <h1> 404 Not Found</h1>,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ advance-routing/src/view/ReadPost/Readpost.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Readpost() {
+  return (
+    <div>
+      <h1>
+        Readpost
+      </h1>
+    </div>
+  )
+}
+
+export default Readpost
